d55c98452776fa35af4a01a19a86bf4474388f31
Chammi Weerasekera <Chammi.Weerasekera@fglsports.com>
2015-09-02 19:39:33 -0600

    Reconfigured project to deploy to heroku

--- src/main/java/com/appdirect/backend/rest/controllers/EventController.java
+++ src/main/java/com/appdirect/backend/rest/controllers/EventController.java
@@ -12,7 +12,6 @@ import com.appdirect.backend.rest.resources.EventListResource;
 import com.appdirect.backend.rest.resources.EventResource;
 import com.appdirect.backend.rest.resources.asm.EventListResourceAsm;
 import com.appdirect.backend.rest.resources.asm.EventResourceAsm;
-import com.google.protos.cloud.sql.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +19,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.net.URI;
 
